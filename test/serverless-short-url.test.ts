8c0ff34ff774e2ee8edba2bd7d791ed1f1c8da52
ta-kato2 <ta-kato2@unext.jp>
2022-09-22 16:22:04 +0900

    Initial commit

---
+++ test/serverless-short-url.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessShortUrl from '../lib/serverless-short-url-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-short-url-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessShortUrl.ServerlessShortUrlStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
